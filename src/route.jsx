5225c0a6345d42f67bacba97d7b5420a12e92134
Chris <nzeanoruec@gmail.com>
2023-05-28 00:25:55 +0100

    added HomePage back

--- src/route.jsx
+++ src/route.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ModalPage from "./components/ModalPage";
+import MainPageMenu from "./pages/MainPageMenu";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import DataAndPrivacy from "./pages/dataAndPrivacyPage/DataAndPrivacy";
 import ErrorPage from "./pages/errorPage/ErrorPage";
-// import HomePage from "./pages/homePage/HomePage";
+import HomePage from "./pages/homePage/HomePage";
 import PaymentPage from "./pages/paymentAndSubscriptionPage/PaymentPage";
 import PersonalPage from "./pages/personalAndSharingPage/PersonalPage";
 import InfoPage from "./pages/personalInfoPage/InfoPage";
 import SecurityPage from "./pages/securityPage/SecurityPage";
-import MainPageMenu from "./pages/MainPageMenu";
 
 const router = createBrowserRouter([
 	{
@@ -21,8 +21,8 @@ const router = createBrowserRouter([
 				path: "/",
 				element: <MainPageMenu />,
 				children: [
-					{ path: "/", element: <AboutPage /> },
-					// { path: "/aboutUs", element: <AboutPage /> },
+					{ path: "/", element: <HomePage /> },
+					{ path: "/aboutUs", element: <AboutPage /> },
 					{ path: "/payment", element: <PaymentPage /> },
 					{ path: "/personal-and-sharing", element: <PersonalPage /> },
 					{ path: "/personal-info", element: <InfoPage /> },
