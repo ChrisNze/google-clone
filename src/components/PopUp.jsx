2f3a9e1b398196b834302d6c759f674f2ea71ac5
Chris <nzeanoruec@gmail.com>
2023-05-29 17:02:25 +0100

    added border to popUp on darkMode

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -3,11 +3,13 @@ import popUpImg from "../assets/popUpImg.png";
 import useZustand from "../hooks/useZustand";
 import PopUpCloseMessage from "./PopUpCloseMessage";
 const PopUp = () => {
-	const { popUp, popUpOff, setSmShow } = useZustand();
+	const { darkMode, popUp, popUpOff, setSmShow } = useZustand();
 	return (
 		<>
 			{popUp ? (
-				<div className="col-11 col-sm-7 col-xl-8 col-xxl-7 pt-3 pb-5 px-4 px-sm-5 mx-auto mt-5 popUpContainer">
+				<div
+					className="col-11 col-sm-7 col-xl-8 col-xxl-7 pt-3 pb-5 px-4 px-sm-5 mx-auto mt-5 popUpContainer"
+					style={{ border: !darkMode && "1px solid rgba(255, 255, 255, 0.3)" }}>
 					<div className="d-flex justify-content-between align-items-center mb-3">
 						<h1 className="" style={{ fontSize: "2.3rem" }}>
 							Safer sign-in with Google
