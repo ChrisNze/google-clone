b790e7a9ca448d3210f3c870982bcdac8b86abc7
Chris <nzeanoruec@gmail.com>
2023-06-01 08:12:50 +0100

    testing

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
 	const message = isRouteErrorResponse(errorMessage);
 	return (
 		<div className="w-50 mx-auto mt-5">
-			<h1 className="display-1">Oops...................</h1>
+			<h1 className="display-1">Oops..................</h1>
 			<h1 className="display-2">{message ? "Page Not Found" : "Unexpected Error"}</h1>
 			<button className="btn btn-primary mt-5">
 				<Link to="/" className="text-light">
